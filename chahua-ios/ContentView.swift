3bd8e4e728f0bd5ad4d8431df8494aec62ef43a2
codetector <codetector@codetector.org>
2026-08-23 17:47:32 -0700

    Initial Commit

---
+++ chahua-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chahua-ios
+//
+//  Created by Codetector on 8/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chahua-ios/chahua_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chahua_iosApp.swift
+//  chahua-ios
+//
+//  Created by Codetector on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct chahua_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
